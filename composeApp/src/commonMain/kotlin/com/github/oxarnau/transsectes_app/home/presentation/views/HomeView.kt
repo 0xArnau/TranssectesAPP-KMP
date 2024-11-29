50d44557d6e0df0ea42a686aa7de42a7c082b5c1
Arnau <>
2024-11-29 17:22:53 +0100

    refactor(HomeView): move `TextImageButton` & `ImageTextButton` to its own component

--- composeApp/src/commonMain/kotlin/com/github/oxarnau/transsectes_app/home/presentation/views/HomeView.kt
+++ composeApp/src/commonMain/kotlin/com/github/oxarnau/transsectes_app/home/presentation/views/HomeView.kt
@@ -4,32 +4,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.DrawableResource
+import com.github.oxarnau.transsectes_app.shared.components.ImageTextButton
+import com.github.oxarnau.transsectes_app.shared.components.TextImageButton
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import transsectesapp.composeapp.generated.resources.Res
@@ -132,96 +123,4 @@ fun ScrollContent(innerPadding: PaddingValues) {
 }
 
 
-@Composable
-fun ImageTextButton(
-    resourceId: DrawableResource,
-    resourceDescription: String,
-    text: String,
-    modifier: Modifier = Modifier,
-    size: Dp = 128.dp,
-    onClick: () -> Unit,
-) {
-    val contentColor =
-        if (isSystemInDarkTheme()) Color.White else Color.Black
 
-
-    Button(
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent,
-            contentColor = contentColor
-        ),
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth()
-        ) {
-
-            Image(
-                painter = painterResource(resourceId),
-                contentDescription = resourceDescription,
-                modifier = Modifier
-                    .padding(end = 16.dp)
-                    .size(size)
-
-            )
-
-            Text(
-                text = text,
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
-    }
-}
-
-@Composable
-fun TextImageButton(
-    resourceId: DrawableResource,
-    resourceDescription: String,
-    text: String,
-    modifier: Modifier = Modifier,
-    size: Dp = 128.dp,
-    onClick: () -> Unit,
-) {
-    val contentColor =
-        if (isSystemInDarkTheme()) Color.White else Color.Black
-
-
-    Button(
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent,
-            contentColor = contentColor
-        ),
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth()
-        ) {
-
-            Text(
-                text = text,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(end = 16.dp)
-            )
-
-            Image(
-                painter = painterResource(resourceId),
-                contentDescription = resourceDescription,
-                modifier = Modifier
-                    .size(size)
-            )
-
-        }
-    }
-}
---
+++ composeApp/src/commonMain/kotlin/com/github/oxarnau/transsectes_app/shared/components/TextImageButton.kt
@@ -0,0 +1,69 @@
+package com.github.oxarnau.transsectes_app.shared.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun TextImageButton(
+    resourceId: DrawableResource,
+    resourceDescription: String,
+    text: String,
+    modifier: Modifier = Modifier,
+    size: Dp = 128.dp,
+    onClick: () -> Unit,
+) {
+    val contentColor =
+        if (isSystemInDarkTheme()) Color.White else Color.Black
+
+
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = contentColor
+        ),
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(end = 16.dp)
+            )
+
+            Image(
+                painter = painterResource(resourceId),
+                contentDescription = resourceDescription,
+                modifier = Modifier
+                    .size(size)
+            )
+
+        }
+    }
+}
