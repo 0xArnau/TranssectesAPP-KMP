25fbc18eb894ab7088c0699562b3d1f4f1488504
Arnau <>
2024-12-11 12:03:19 +0100

    feat(edge2edge): enable `edgeToEdge` remove black status & navigation bar
    TODO: do the same on iOS

--- composeApp/src/androidMain/kotlin/com/github/oxarnau/transsectes_app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/github/oxarnau/transsectes_app/MainActivity.kt
@@ -3,13 +3,17 @@ package com.github.oxarnau.transsectes_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.oxarnau.transsectes_app.app.App
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge() // TODO: iOS
 
         setContent {
             App()
